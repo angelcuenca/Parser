465856d78a02fb74c396ab00430b77f499903e12
Ángel Sánchez <angel.esc@hotmail.com>
2016-06-14 09:22:02 -0500

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,22 +1,30 @@
-/* Sanchez Cuenca Angel Emmanuel
- * Compiladores
- * Proyecto Final
- * Parser RSS
- * Diciembre 2013
- */
+/** Copyright (c) 2013 Angel Sanchez 
+ *  angelscrf@gmail.com
+ *  Compiladores - Proyecto - Parser RSS
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the Software), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+*/
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 using namespace std;
-
 fstream ficheroEntrada;
 
 class Lexico{
 
 public:
-	
 	bool continua;
 	bool noSiguienteCaracter;
 	bool contenido;
